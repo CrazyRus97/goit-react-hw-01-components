1c333a0cc5e1c5d03f763f6bd193ab403abf4afc
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2023-10-15 18:25:50 +0300

    end2

--- src/components/statistics/Statistic.jsx
+++ src/components/statistics/Statistic.jsx
@@ -16,7 +16,6 @@ export const Statistics = ({ stats }) => {
 
         <ul className={css.statList}>
             {stats.map((item, idx) => (
-                
                 <li key={item.id} id={item.id} index={idx} className={css.item} style={randomColor}>
                     <span className={css.label}>{item.label}</span>
                     <span className={css.percentage}>{item.percentage}%</span>
