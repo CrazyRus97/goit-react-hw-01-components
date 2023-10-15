098d00f427ade3f327cedf341a435d1ba76a163e
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2023-10-15 16:15:38 +0300

    task 1+2

---
+++ src/components/statistics/Statistic.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import css from './Statistic.module.css'
+
+const getRandomHexColor = idx =>
+  `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+
+const randomColor = {
+    backgroundColor: getRandomHexColor()
+};
+
+export const Statistics = ({ stats }) => {
+    return (
+    <section className={css.statistics}>
+        <h2 className={css.title}>Upload stats</h2>
+
+        <ul className={css.statList}>
+            {stats.map((item, idx) => (
+                
+                <li key={item.id} id={item.id} index={idx} className={css.item} style={randomColor}>
+                    <span className={css.label}>{item.label}</span>
+                    <span className={css.percentage}>{item.percentage}%</span>
+                </li>
+            ))}
+        </ul>
+    </section>
+  )
+}
